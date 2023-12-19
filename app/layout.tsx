4e76a80edaed2a50234a72bf16278362db3af10a
Mohammed Mudassir ullah Khan <mudassir222001@gmail.com>
2023-12-19 13:05:40 +0530

    ✨Feat : implement Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Outfit } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/layout/Navbar/Navbar'
+import Footer from '@/components/layout/Footer/Footer'
 
 const font = Outfit({ subsets: ['latin'] })
 
@@ -20,7 +21,7 @@ export default function RootLayout({
       <body className={font.className}>
         <Navbar />
         {children}
-
+        <Footer />
       </body>
     </html>
   )
